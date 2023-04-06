e9ba3e524d86aa8c1bef1513191228d640334ce4
giselemilagros88 <92891602+giselemilagros88@users.noreply.github.com>
2023-04-06 17:31:28 -0300

    primer commit

---
+++ data/stock.js
@@ -0,0 +1,133 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Alice",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, Alice.",
+        precio: 1200,
+        imagen: '../public/images/Alice.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "DarthVader",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, DarthVader.",
+        precio: 1200,
+        imagen: '../public/images/darthvader.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Django",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, Django.",
+        precio: 1200,
+        imagen: '../public/images/django.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Groot",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, Groot.",
+        precio: 1200,
+        imagen: '../public/images/groot.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "it",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, It.",
+        precio: 1200,
+        imagen: '../public/images/it.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Leia",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, Leia.",
+        precio: 1200,
+        imagen: '../public/images/leia.jpg',
+        cantidad:1
+    },
+    
+    {
+        id: 7,
+        nombre: "Orange",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, Orange.",
+        precio: 1200,
+        imagen: '../public/images/orange.jpg',
+        cantidad:1
+    },
+    
+    {
+        id: 8,
+        nombre: "Ouatih",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, Ouatih.",
+        precio: 1200,
+        imagen: '../public/images/ouatih.jpg',
+        cantidad:1
+    },
+    
+    {
+        id: 9,
+        nombre: "Potter2",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, Potter2.",
+        precio: 1200,
+        imagen: '../public/images/potter2.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 10,
+        nombre: "Pulp Fiction Hairs",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, PulpFictionHairs.",
+        precio: 1200,
+        imagen: '../public/images/pupFictionHairs.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 11,
+        nombre: "The God father",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine, The God father.",
+        precio: 1200,
+        imagen: '../public/images/theGodfather.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 12,
+        nombre: "The Mask",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine,The Mask.",
+        precio: 1200,
+        imagen: '../public/images/theMask.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 13,
+        nombre: "The Office",
+        tipo: "Lisa",
+        desc: "Libreta Artesanal Cine,The Office.",
+        precio: 1200,
+        imagen: '../public/images/theOffice.jpg',
+        cantidad:1
+    }
+    
+];
